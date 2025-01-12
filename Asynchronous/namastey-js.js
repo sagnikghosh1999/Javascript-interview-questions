945cbb3506c5eb5a39000198cc71b18619bb7b66
sagnikghosh1999 <saggywebd@gmail.com>
2025-01-12 14:44:36 +0530

    Promises API

--- Asynchronous/namastey-js.js
+++ Asynchronous/namastey-js.js
@@ -61,7 +61,7 @@ createOrder(cart)
   .then(function () {
     redirect();
   });
-*/
+
 
 const cart = ["a", "b", "c"];
 
@@ -96,5 +96,75 @@ createOrder(cart)
     console.log(data);
   });
 
+  */
 //Promises API
-//1. Promise.ALL
+//1. Promise.ALL -->when all promises succeeds or any one fails
+
+const p1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    // resolve("P1 resolved ");
+    reject("P1 failed ");
+  }, 3000);
+});
+
+const p2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    // resolve("P2 resolved ");
+    reject("P2 failed ");
+  }, 1000);
+});
+
+const p3 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    // resolve("P3 resolved ");
+    reject("P3 failed ");
+  }, 2000);
+});
+
+// Promise.all
+console.time("all");
+// Promise.all resolves when all promises resolve, or rejects if any promise rejects
+Promise.all([p1, p2, p3])
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err))
+  .then(() => console.timeEnd("all"));
+
+//  Promise.allSettled
+console.time("all-settled");
+// Promise.allSettled resolves when all promises settle (either resolve or reject)
+Promise.allSettled([p1, p2, p3])
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err))
+  .then(() => console.timeEnd("all-settled"));
+/**
+ * [
+    {
+        "status": "fulfilled",
+        "value": "P1 resolved "
+    },
+    {
+        "status": "fulfilled",
+        "value": "P2 resolved "
+    },
+    {
+        "status": "rejected",
+        "reason": "P3 failed "
+    }
+  ]
+ */
+
+//  Promise.race
+console.time("race");
+// Promise.race resolves or rejects as soon as one of the promises resolves or rejects
+Promise.race([p1, p2, p3])
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err))
+  .then(() => console.timeEnd("race"));
+
+//  Promise.any
+console.time("any");
+// Promise.any resolves as soon as one of the promises resolves, or rejects if all promises reject
+Promise.any([p1, p2, p3])
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err.errors)) // AggregateError: All promises were rejected
+  .then(() => console.timeEnd("any"));
